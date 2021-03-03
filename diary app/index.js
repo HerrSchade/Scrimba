42a68288017f9bf69930f17d8d490409f4c691c7
HerrSchade <oskartdragon@gmail.com>
2021-03-03 22:06:42 +0000

    Finished at entry section

--- diary app/index.js
+++ diary app/index.js
@@ -1,18 +1,31 @@
-let entryForm = document.getElementById('submit-button');
-let entriesSection = document.getElementById("entries");
-let entriesTextbox = document.getElementsByClassName("entry-textbox");
+let entryForm = document.getElementById('entryForm');
+let entriesSection = document.querySelector("#entries");
+let entriesTextbox = document.querySelector(".entry-textbox");
+let entriesNav = document.querySelector(".entries-nav")
 
 
 
-
+let count = 1
 function addEntryToDom(event) {
-     event.preventDefault()
-     let entryDiv = document.createElement("div")
-     entryDiv.classname = "single-entry"
-     entryDiv.textContent = entriesTextbox[0].value
-     entriesSection.append(entryDiv)
-     entriesTextbox[0].value = ""
+     event.preventDefault();
+     let entryDiv = document.createElement("div");
+     entryDiv.classname = "single-entry";
+     entryDiv.textContent = entriesTextbox.value;
+     entryDiv.style.display = "none";
+     entriesSection.append(entryDiv);
+     entriesTextbox.value = "";
+     
+     let displayEntryButton = document.createElement("button")
+     displayEntryButton.className = 'display-entry-button';
+     displayEntryButton.textContent = count;
+     entriesNav.append(displayEntryButton)
 
+     displayEntryButton.addEventListener("click", function() {
+          entryDiv.style.display = "block"
+     })
+
+     count++
 }
-entryForm = addEventListener("submit", addEntryToDom)
+entryForm.addEventListener("submit", addEntryToDom)
+
 
